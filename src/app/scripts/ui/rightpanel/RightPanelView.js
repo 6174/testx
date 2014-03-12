98a113de8456e90db4a12100095925c5b77b40c7
6174 <570171025@qq.com>
2014-03-12 09:48:44 +0800

    修复ace editor没有根据弹性布局自动resize的问题

--- src/app/scripts/ui/rightpanel/RightPanelView.js
+++ src/app/scripts/ui/rightpanel/RightPanelView.js
@@ -2,64 +2,66 @@
  * Right panelView
  */
 var util = require('util'),
-	hljs = require('highlight.js');
-
+    hljs = require('highlight.js');
 var RightPanelView = Backbone.View.extend({
-	el: '#TxRightPanel',
-	events: {
-		'click #TxConsoleClearTrigger': 'clear'
-	},
-	initialize: function(){
-		this.logEl = this.$('#TxConsole'); 
-		this.initEditor();
-	},
-	initEditor: function(){
-	    var self = this;
-		var editor = ace.edit("TxCodeViewer");
-	    editor.setPrintMarginColumn(false);
-	    editor.setShowFoldWidgets(false);
-	    editor.getSession().setMode("ace/mode/javascript");
-	    editor.getSession().setTabSize(2);
-	    // editor.renderer.setShowGutter(false);
-	    editor.getSession().setUseWorker(false);
-	    // editor.setReadOnly(true);
-	    self.editor = editor;
-	    window.editor = editor;
-	    var code = '';
-	    mediator.on('combo-script', function(ev){
-	    	code = ev.code;
-	    });
-
-	    $('body').delegate('.script-view-link', 'click', function(ev){
-	    	var line = $(this).data('line'),
-	    		row = $(this).data('row');
-	    	// console.log('click', line, row);
-	    	self.showCodeViewPanel({
-	    		code: code,
-	    		line: line,
-	    		row: row
-	    	});
-	    });
-	},
-	showCodeViewPanel: function(conf){
-		conf = conf || {};
-		this.$('#TabCodeViewer').tab('show');
-		if(conf.code) {
-			this.editor.setValue(conf.code);
-		}
-		if(conf.line) {
-			this.editor.gotoLine(Number(conf.line));
-		}
-	},
-	log: function(data){
-		var node = $('<p class="log-line"></p>');
-		node.html('> ' + hljs.highlight('javascript', data).value);			
-		this.logEl.prepend(node);
-	},
-	clear: function(){
-		console.log('asdfasdf');
-		this.logEl.html('');
-	}
+    el: '#TxRightPanel',
+    events: {
+        'click #TxConsoleClearTrigger': 'clear'
+    },
+    initialize: function() {
+        this.logEl = this.$('#TxConsole');
+        this.initEditor();
+    },
+    initEditor: function() {
+        var self = this;
+        var editor = ace.edit("TxCodeViewer");
+        editor.setPrintMarginColumn(false);
+        editor.setShowFoldWidgets(false);
+        editor.getSession().setMode("ace/mode/javascript");
+        editor.getSession().setTabSize(2);
+        // editor.renderer.setShowGutter(false);
+        editor.getSession().setUseWorker(false);
+        editor.setReadOnly(true);
+        self.editor = editor;
+        window.editor = editor;
+        window.addEventListener('resize', function() {
+            setTimeout(function(){
+	            editor.resize();
+            }, 10);
+        });
+        var code = '';
+        mediator.on('combo-script', function(ev) {
+            code = ev.code;
+        });
+        $('body').delegate('.script-view-link', 'click', function(ev) {
+            var line = $(this).data('line'),
+                row = $(this).data('row');
+            // console.log('click', line, row);
+            self.showCodeViewPanel({
+                code: code,
+                line: line,
+                row: row
+            });
+        });
+    },
+    showCodeViewPanel: function(conf) {
+        conf = conf || {};
+        this.$('#TabCodeViewer').tab('show');
+        if (conf.code) {
+            this.editor.setValue(conf.code);
+        }
+        if (conf.line) {
+            this.editor.gotoLine(Number(conf.line));
+        }
+    },
+    log: function(data) {
+        var node = $('<p class="log-line"></p>');
+        node.html('> ' + hljs.highlight('javascript', data).value);
+        this.logEl.prepend(node);
+    },
+    clear: function() {
+        console.log('asdfasdf');
+        this.logEl.html('');
+    }
 });
-
 module.exports = RightPanelView;
