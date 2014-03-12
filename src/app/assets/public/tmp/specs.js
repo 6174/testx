a5ddac9b1c0f4b774a9243459ad793c3aa34350f
6174 <570171025@qq.com>
2014-03-12 09:17:44 +0800

    添加说明引导， 框架布局全部改为flex

--- src/app/assets/public/tmp/specs.js
+++ src/app/assets/public/tmp/specs.js
@@ -6204,38 +6204,117 @@ if (typeof define === "function" && define.amd) {
 })();
 
 /******************/
+describe("Player", function() {
+  var player;
+  var song;
+
+  beforeEach(function() {
+    player = new Player();
+    song = new Song();
+  });
+
+  it("should be able to play a Song", function() {
+    player.play(song);
+    expect(player.currentlyPlayingSong).toEqual(song);
+
+    //demonstrates use of custom matcher
+    expect(player).toBePlaying(song);
+  });
+
+  describe("when song has been paused", function() {
+    beforeEach(function() {
+      player.play(song);
+      player.pause();
+      console.log(' before each pause the song!');
+    });
+
+    it("should indicate that the song is currently paused", function() {
+      expect(player.isPlaying).toBeFalsy();
+
+      // demonstrates use of 'not' with a custom matcher
+      expect(player).not.toBePlaying(song);
+    });
+
+    it("should be possible to resume", function() {
+      player.resume();
+      expect(player.isPlaying).toBeTruthy();
+      expect(player.currentlyPlayingSong).toEqual(song);
+    });
+  });
+
+  // demonstrates use of spies to intercept and test method calls
+  it("tells the current song if the user has made it a favorite", function() {
+    spyOn(song, 'persistFavoriteStatus');
+
+    player.play(song);
+    player.makeFavorite();
+
+    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  });
+
+  //demonstrates use of expected exceptions
+  describe("#resume", function() {
+    it("should throw an exception if song is already playing", function() {
+      player.play(song);
+
+      expect(function() {
+        player.resume();
+      }).toThrowError("song is already playing");
+    });
+  });
+});
 
 
 /******************/
-describe('TodoModel', function(){
-	var model;
-	 
-	beforeEach(function() {
-		expect(app).not.toBe(null);
-	    model = new app.TodoModel();
-	    console.log('model: ', model);
-	});
+beforeEach(function () {
+  jasmine.addMatchers({
+    toBePlaying: function () {
+      return {
+        compare: function (actual, expected) {
+          var player = actual;
 
-	it('model should not be null', function(){
-		expect(model).not.toBe(null);
-	});
+          return {
+            pass: player.currentlyPlayingSong === expected && player.isPlaying
+          }
+        }
+      };
+    }
+  });
+});
 
-	it('model default values should equal title:default_tile, complete:complete', function(){
-		expect(model.get('title')).toEqual('default_title');
-		expect(model.get('completed')).toEqual(false);
-	});
 
-	it('instance model with config', function(){
-		var title = 'title from config';
-		model = new app.TodoModel({title: title});
-		expect(model.get('title')).toNotEqual(title);
-	});
+/******************/
+function Player() {
+}
+Player.prototype.play = function(song) {
+  this.currentlyPlayingSong = song;
+  this.isPlaying = true;
+};
 
-	it('after toggle called complete should be toggle', function(){
-		model.toggle();
-		expect(model.get('completed')).toBeTruthy();
-	});
-});	
+Player.prototype.pause = function() {
+  this.isPlaying = false;
+};
+
+Player.prototype.resume = function() {
+  if (this.isPlaying) {
+    throw new Error("song is already playing");
+  }
+
+  this.isPlaying = true;
+};
+
+Player.prototype.makeFavorite = function() {
+  this.currentlyPlayingSong.persistFavoriteStatus(true);
+};
+
+/******************/
+function Song() {
+}
+
+Song.prototype.persistFavoriteStatus = function(value) {
+  // something complicated
+  throw new Error("not yet implemented");
+};
 
 /******************/
 (function(){
